156c82f71884b9330ebf7de30e68fd4910b6536a
Vidal Romain <romain1.vidal@epita.fr>
2025-11-19 10:40:15 +0100

    Map

--- ProjetJV2025/ProjetJV2025/Assets/Scripts/Object/BuildSpawner.cs
+++ ProjetJV2025/ProjetJV2025/Assets/Scripts/Object/BuildSpawner.cs
@@ -1,39 +1,56 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BuildSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject canvas;
     [SerializeField] private TMP_Text buildNameText;
+    [SerializeField] private TMP_Text bottomText;
     [SerializeField] private GameObject[] buildPrefabs;
+    [SerializeField] private float spawnTimer = 20f;
+    [SerializeField] private float cooldownTimer = 90f;
 
     private int curIndex = 0;
     private float timer = 2f;
+    private bool inCooldown = false;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         canvas.SetActive(false);
         buildNameText.text = buildPrefabs[curIndex].name;
+        bottomText.text = "E";
     }
 
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log($"Build Spawner: {other.name}");
         if (other.CompareTag("Player"))
         {
             canvas.SetActive(true);
             canvas.transform.LookAt(Camera.main.transform);
-        }
 
-        if (timer <= 0f)
-        {
-            timer = 2f;
-            curIndex = (curIndex + 1) % buildPrefabs.Length;
-        }
-        else
-        {
-            timer -= Time.deltaTime;
+            if (!inCooldown)
+            {
+                if (timer <= 0f)
+                {
+                    timer = 2f;
+                    curIndex = (curIndex + 1) % buildPrefabs.Length;
+                    buildNameText.text = buildPrefabs[curIndex].name;
+                }
+                else
+                {
+                    timer -= Time.deltaTime;
+                }
+
+                if (Keyboard.current.eKey.wasPressedThisFrame)
+                {
+                    SpawnBuild();
+                }
+            }
         }
     }
 
@@ -48,7 +65,34 @@ public class BuildSpawner : MonoBehaviour
     public void SpawnBuild()
     {
         var position = new Vector3(transform.position.x, 0, transform.position.z);
-        Instantiate(buildPrefabs[curIndex], position, Quaternion.identity);
-        Destroy(gameObject);
+        var build = Instantiate(buildPrefabs[curIndex], transform);
+        inCooldown = true;
+
+        StartCoroutine(CooldownCoroutine(build));
+    }
+
+    private IEnumerator CooldownCoroutine(GameObject build)
+    {
+        float time = spawnTimer;
+
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            bottomText.text = $"{(int)time}s before despawn.";
+            yield return new WaitForEndOfFrame();
+        }
+
+        Destroy(build);
+
+        time = cooldownTimer;
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            bottomText.text = $"Usable in {(int)time}s.";
+            yield return new WaitForEndOfFrame();
+        }
+
+        inCooldown = false;
+        bottomText.text = "E";
     }
 }
